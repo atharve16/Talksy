db4a8a4d46cf45e8c3d252e5331c73eaf80560a2
Atharve Agrawal <atharve211@gmail.com>
2025-03-02 17:39:10 +0530

    fixed file paths

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { UserProvider } from "./context/UserContext.jsx";
-import { ChatProvider } from "./context/ChatContext.jsx";
+import { ChatProvider } from "./context/ChatContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
